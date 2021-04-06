6cc0d019b8b8ddc3998092160a7f6d8c7f5d0721
chrisvasqm <christianvasquezmelo@gmail.com>
2021-04-06 18:39:01 -0400

    Add employee entity

---
+++ src/main/java/com/chrisvasqm/demorestapi/models/Employee.java
@@ -0,0 +1,46 @@
+package com.chrisvasqm.demorestapi.models;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@DynamicUpdate
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
