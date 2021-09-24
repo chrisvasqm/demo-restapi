fb9033d8ffce39ed4a151e364617f15cc6ee5c34
chrisvasqm <christianvasquezmelo@gmail.com>
2021-09-23 21:21:15 -0400

    Add get all employees API

---
+++ src/main/java/com/chrisvasqm/demorestapi/controllers/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.chrisvasqm.demorestapi.controllers;
+
+import com.chrisvasqm.demorestapi.models.Employee;
+import com.chrisvasqm.demorestapi.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping("/employees")
+    public List<Employee> getAll() {
+        return service.getAll();
+    }
+
+}
