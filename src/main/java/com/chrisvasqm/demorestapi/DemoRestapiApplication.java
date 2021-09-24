332be979468de5dc55cf9027ab5c29fbd568cdc1
chrisvasqm <christianvasquezmelo@gmail.com>
2021-09-23 21:15:27 -0400

    Print employee names

--- src/main/java/com/chrisvasqm/demorestapi/DemoRestapiApplication.java
+++ src/main/java/com/chrisvasqm/demorestapi/DemoRestapiApplication.java
@@ -1,13 +1,28 @@
 package com.chrisvasqm.demorestapi;
 
+import com.chrisvasqm.demorestapi.models.Employee;
+import com.chrisvasqm.demorestapi.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DemoRestapiApplication {
 
+	@Autowired
+	private EmployeeService service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRestapiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void postInit() {
+		for (Employee employee : service.getAll()) {
+			System.out.println(employee.getName());
+		}
 	}
 
 }
