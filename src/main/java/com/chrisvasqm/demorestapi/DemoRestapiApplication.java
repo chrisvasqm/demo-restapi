7ece119dcb7d588c95d4ebd0b1e68a95e4e128d7
chrisvasqm <christianvasquezmelo@gmail.com>
2021-09-23 21:30:24 -0400

    Remove DB test print statements

--- src/main/java/com/chrisvasqm/demorestapi/DemoRestapiApplication.java
+++ src/main/java/com/chrisvasqm/demorestapi/DemoRestapiApplication.java
@@ -1,12 +1,9 @@
 package com.chrisvasqm.demorestapi;
 
-import com.chrisvasqm.demorestapi.models.Employee;
 import com.chrisvasqm.demorestapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DemoRestapiApplication {
@@ -16,13 +13,6 @@ public class DemoRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRestapiApplication.class, args);
-	}
-
-	@PostConstruct
-	public void postInit() {
-		for (Employee employee : service.getAll()) {
-			System.out.println(employee.getName());
-		}
 	}
 
 }
